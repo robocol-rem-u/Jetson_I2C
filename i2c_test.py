ec1de59d3705f331db6340ca861fe995f3176bb2
erc <eloyabmfcv@gmail.com>
2021-10-26 15:29:04 -0500

    successfull test

--- i2c_test.py
+++ i2c_test.py
@@ -1,7 +1,14 @@
 from smbus2 import SMBus
+import time
 
 # Open i2c bus 1 and read one byte from address 80, offset 0
 with SMBus(1) as bus:
     # Write a byte to address 80, offset 0
-    data = 0xFF
-    bus.write_byte_data(80, 0, data)
+    while True : 
+        address = 0x20
+        data = 0xFF
+        bus.write_byte_data(address, 0, data)
+        time.sleep(0.2)
+        bus.write_byte_data(address, 0,0x00 )
+        time.sleep(0.2)
+
