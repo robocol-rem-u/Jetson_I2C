8f28b7c8cc49ad0133700385dee9542e542ad1c1
salogranada <s.granada@uniandes.edu.co>
2021-10-28 11:05:15 -0500

    i2c integration with ros

---
+++ i2c_test_ros.py
@@ -0,0 +1,29 @@
+from smbus2 import SMBus
+import time
+import rospy
+
+# Open i2c bus 1 and read one byte from address 80, offset 0
+
+def main_i2c():
+
+    rospy.init_node('i2c_test', anonymous=True) #Inicio nodo
+    rate = rospy.Rate(10) #10hz
+    
+
+    with SMBus(1) as bus:
+        # Write a byte to address 80, offset 0
+        while not rospy.is_shutdown():
+            address = 0x20
+            data = 0xFF
+            bus.write_byte_data(address, 0, data)
+            time.sleep(0.2)
+            bus.write_byte_data(address, 0,0x00 )
+            time.sleep(0.2)
+            
+            rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		main_i2c()
+	except rospy.ROSInterruptException:
+		print('Nodo detenido')
