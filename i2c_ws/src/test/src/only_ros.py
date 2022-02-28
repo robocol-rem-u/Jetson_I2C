0896fa03f7774f9fa3b0613686e064fcef526c66
Danielfts <79421450+Danielfts@users.noreply.github.com>
2022-02-28 10:50:56 -0500

    Fixed minor (but annoying) bug

--- i2c_ws/src/test/src/only_ros.py
+++ i2c_ws/src/test/src/only_ros.py
@@ -8,7 +8,7 @@ def main():
     rate = rospy.Rate(10)
     pub = rospy.Publisher('Robocol/Power/voltages', String, queue_size=10)
 
-    while not rospy.is_shutdown:
+    while not rospy.is_shutdown():
         for i in range(12):
             v = i
             pub.publish('1:{}'.format(v))
